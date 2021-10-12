ba4637cd3029217caabacfecf0d05b3f48f4b369
Elias Castañeda <e71az@icloud.com>
2021-10-12 15:28:14 -0600

    Fix: React category for the button categories and window not closing when a category is clicked

--- js/front.js
+++ js/front.js
@@ -1,247 +1,246 @@
 $(function () {
+  /* =========================================
+   * tooltip
+   *  =======================================*/
 
-    /* =========================================
-     * tooltip
-     *  =======================================*/
+  $('.customer img').tooltip();
 
-    $('.customer img').tooltip();
+  /* =========================================
+   * counters
+   *  =======================================*/
 
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1000,
+  });
 
-    /* =========================================
-     * counters
-     *  =======================================*/
+  /* =================================================
+   * Preventing URL update on navigation link click
+   *  ==============================================*/
 
-    $('.counter').counterUp({
-        delay: 10,
-        time: 1000
+  $('.link-scroll').on('click', function (e) {
+    var anchor = $(this);
+    $('html, body')
+      .stop()
+      .animate(
+        {
+          scrollTop: $(anchor.attr('href')).offset().top,
+        },
+        1000,
+      );
+    e.preventDefault();
+  });
+
+  /* =========================================
+   *  Scroll Spy
+   *  =======================================*/
+
+  $('body').scrollspy({
+    target: '#navbarcollapse',
+    offset: 80,
+  });
+
+  /* =========================================
+   * testimonial slider
+   *  =======================================*/
+
+  $('.testimonials').owlCarousel({
+    nav: false,
+    dots: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 3,
+      },
+      1200: {
+        items: 4,
+      },
+    },
+  });
+
+  /* =========================================
+   * Leflet map
+   *  =======================================*/
+  map();
+
+  /* =========================================
+   * parallax
+   *  =======================================*/
+  $(window).scroll(function () {
+    var scroll = $(this).scrollTop();
+
+    if ($(window).width() > 1250) {
+      $('.parallax').css({
+        'background-position': 'left -' + scroll / 8 + 'px',
+      });
+    } else {
+      $('.parallax').css({
+        'background-position': 'center center',
+      });
+    }
+  });
+
+  /* =========================================
+   * filter
+   *  =======================================*/
+
+  $('#filter a').click(function (e) {
+    e.preventDefault();
+
+    $('#filter li').removeClass('active');
+    $(this).parent('li').addClass('active');
+
+    var categoryToFilter = $(this).attr('data-filter');
+
+    $('.reference-item').each(function () {
+      if (
+        $(this).data('category') === categoryToFilter ||
+        categoryToFilter === 'all'
+      ) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
     });
+  });
 
-    /* =================================================
-     * Preventing URL update on navigation link click
-     *  ==============================================*/
+  /* =========================================
+   * reference functionality
+   *  =======================================*/
+  $('.reference a').on('click', function (e) {
+    e.preventDefault();
 
-    $('.link-scroll').on('click', function (e) {
-        var anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $(anchor.attr('href')).offset().top
-        }, 1000);
-        e.preventDefault();
-    });
+    var title = $(this).find('.reference-title').text(),
+      description = $(this).siblings('.reference-description').html();
 
+    $('#detail-title').text(title);
+    $('#detail-content').html(description);
 
-    /* =========================================
-     *  Scroll Spy
-     *  =======================================*/
-
-    $('body').scrollspy({
-        target: '#navbarcollapse',
-        offset: 80
-    });
-
-
-    /* =========================================
-     * testimonial slider
-     *  =======================================*/
-
-    $(".testimonials").owlCarousel({
-        nav: false,
-        dots: true,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            1000: {
-                items: 3
-            },
-            1200: {
-                items: 4
-            }
-        }
-    });
-
-
-    /* =========================================
-     * Leflet map
-     *  =======================================*/
-    map();
-
-
-    /* =========================================
-     * parallax
-     *  =======================================*/
-    $(window).scroll(function () {
-
-        var scroll = $(this).scrollTop();
-
-        if ($(window).width() > 1250) {
-            $('.parallax').css({
-                'background-position': 'left -' + scroll / 8 + 'px'
-            });
-        } else {
-            $('.parallax').css({
-                'background-position': 'center center'
-            });
-        }
-    });
-
-    /* =========================================
-     * filter
-     *  =======================================*/
-
-    $('#filter a').click(function (e) {
-        e.preventDefault();
-
-        $('#filter li').removeClass('active');
-        $(this).parent('li').addClass('active');
-
-        var categoryToFilter = $(this).attr('data-filter');
-
-        $('.reference-item').each(function () {
-
-            if ($(this).data('category') === categoryToFilter || categoryToFilter === 'all') {
-                $(this).show();
-            } else {
-                $(this).hide();
-            }
-        });
-
-    });
-
-
-    /* =========================================
-     * reference functionality
-     *  =======================================*/
-    $('.reference a').on('click', function (e) {
-
-        e.preventDefault();
-
-        var title = $(this).find('.reference-title').text(),
-            description = $(this).siblings('.reference-description').html();
-
-        $('#detail-title').text(title);
-        $('#detail-content').html(description);
-
-        var images = $(this).siblings('.reference-description').data('images').split(',');
-        if (images.length > 0) {
-            sliderContent = '';
-            for (var i = 0; i < images.length; ++i) {
-                sliderContent = sliderContent + '<div class="item"><img src=' + images[i] + ' alt="" class="img-fluid"></div>';
-            }
-        } else {
-            sliderContent = '';
-        }
-
-        openReference(sliderContent);
-
-    });
-
-    function openReference(sliderContent) {
-        $('#detail').slideDown();
-        $('#references-masonry').slideUp();
-
-
-        if (sliderContent !== '') {
-
-            var slider = $('#detail-slider');
-
-            if (slider.hasClass('owl-loaded')) {
-                slider.trigger('replace.owl.carousel', sliderContent);
-            } else {
-                slider.html(sliderContent);
-                slider.owlCarousel({
-                    nav: false,
-                    dots: true,
-                    items: 1
-                });
-
-            }
-        }
+    var images = $(this)
+      .siblings('.reference-description')
+      .data('images')
+      .split(',');
+    if (images.length > 0) {
+      sliderContent = '';
+      for (var i = 0; i < images.length; ++i) {
+        sliderContent =
+          sliderContent +
+          '<div class="item"><img src=' +
+          images[i] +
+          ' alt="" class="img-fluid"></div>';
+      }
+    } else {
+      sliderContent = '';
     }
 
+    openReference(sliderContent);
+  });
 
-    function closeReference() {
-        $('#references-masonry').slideDown();
-        $('#detail').slideUp();
+  function openReference(sliderContent) {
+    $('#detail').slideDown();
+    $('#references-masonry').slideUp();
+
+    if (sliderContent !== '') {
+      var slider = $('#detail-slider');
+
+      if (slider.hasClass('owl-loaded')) {
+        slider.trigger('replace.owl.carousel', sliderContent);
+      } else {
+        slider.html(sliderContent);
+        slider.owlCarousel({
+          nav: false,
+          dots: true,
+          items: 1,
+        });
+      }
     }
+  }
 
-    $('#filter button, #detail .close').on('click', function () {
-        closeReference();
-    });
+  function closeReference() {
+    $('#references-masonry').slideDown();
+    $('#detail').slideUp();
+  }
 
+  $('#filter a, #detail .close').on('click', function () {
+    closeReference();
+  });
 
-    /* =========================================
-     *  animations
-     *  =======================================*/
+  /* =========================================
+   *  animations
+   *  =======================================*/
 
-    delayTime = 0;
+  delayTime = 0;
 
-    $('[data-animate]').waypoint(function (direction) {
-        delayTime += 250;
+  $('[data-animate]').waypoint(
+    function (direction) {
+      delayTime += 250;
 
-        var element = $(this.element);
+      var element = $(this.element);
 
-        $(this.element).delay(delayTime).queue(function (next) {
-            element.toggleClass('animated');
-            element.toggleClass(element.data('animate'));
-            delayTime = 0;
-            next();
+      $(this.element)
+        .delay(delayTime)
+        .queue(function (next) {
+          element.toggleClass('animated');
+          element.toggleClass(element.data('animate'));
+          delayTime = 0;
+          next();
         });
 
-        this.destroy();
+      this.destroy();
+    },
+    {
+      offset: '90%',
+    },
+  );
 
-    }, {
-        offset: '90%'
-    });
+  $('[data-animate-hover]').hover(
+    function () {
+      $(this).css({
+        opacity: 1,
+      });
+      $(this).addClass('animated');
+      $(this).removeClass($(this).data('animate'));
+      $(this).addClass($(this).data('animate-hover'));
+    },
+    function () {
+      $(this).removeClass('animated');
+      $(this).removeClass($(this).data('animate-hover'));
+    },
+  );
 
-    $('[data-animate-hover]').hover(function () {
-        $(this).css({
-            opacity: 1
-        });
-        $(this).addClass('animated');
-        $(this).removeClass($(this).data('animate'));
-        $(this).addClass($(this).data('animate-hover'));
-    }, function () {
-        $(this).removeClass('animated');
-        $(this).removeClass($(this).data('animate-hover'));
-    });
+  /* =========================================
+   * for demo purpose
+   *  =======================================*/
 
-    /* =========================================
-     * for demo purpose
-     *  =======================================*/
+  var stylesheet = $('link#theme-stylesheet');
+  $("<link id='new-stylesheet' rel='stylesheet'>").insertAfter(stylesheet);
+  var alternateColour = $('link#new-stylesheet');
 
-    var stylesheet = $('link#theme-stylesheet');
-    $("<link id='new-stylesheet' rel='stylesheet'>").insertAfter(stylesheet);
-    var alternateColour = $('link#new-stylesheet');
+  if ($.cookie('theme_csspath')) {
+    alternateColour.attr('href', $.cookie('theme_csspath'));
+  }
 
-    if ($.cookie("theme_csspath")) {
-        alternateColour.attr("href", $.cookie("theme_csspath"));
+  $('#colour').change(function () {
+    if ($(this).val() !== '') {
+      var theme_csspath = 'css/style.' + $(this).val() + '.css';
+
+      alternateColour.attr('href', theme_csspath);
+
+      $.cookie('theme_csspath', theme_csspath, {
+        expires: 365,
+        path: document.URL.substr(0, document.URL.lastIndexOf('/')),
+      });
     }
 
-    $("#colour").change(function () {
-
-        if ($(this).val() !== '') {
-
-            var theme_csspath = 'css/style.' + $(this).val() + '.css';
-
-            alternateColour.attr("href", theme_csspath);
-
-            $.cookie("theme_csspath", theme_csspath, {
-                expires: 365,
-                path: document.URL.substr(0, document.URL.lastIndexOf('/'))
-            });
-
-        }
-
-        return false;
-    });
-
+    return false;
+  });
 });
-
-
 
 /* =========================================
  * styled Leaflet Map
@@ -251,62 +250,65 @@ $(function () {
 // ------------------------------------------------------ //
 
 function map() {
+  var mapId = 'map',
+    mapCenter = [28.636002277036024, -106.07719306764736],
+    mapMarker = true;
 
-    var mapId = 'map',
-        mapCenter = [28.636002277036024, -106.07719306764736],
-        mapMarker = true;
+  if ($('#' + mapId).length > 0) {
+    var icon = L.icon({
+      iconUrl: 'img/marker.png',
+      iconSize: [25, 37.5],
+      popupAnchor: [0, -18],
+      tooltipAnchor: [0, 19],
+    });
 
-    if ($('#' + mapId).length > 0) {
+    var dragging = false,
+      tap = false;
 
-        var icon = L.icon({
-            iconUrl: 'img/marker.png',
-            iconSize: [25, 37.5],
-            popupAnchor: [0, -18],
-            tooltipAnchor: [0, 19]
-        });
-
-        var dragging = false,
-            tap = false;
-
-        if ($(window).width() > 700) {
-            dragging = true;
-            tap = true;
-        }
-
-        var map = L.map(mapId, {
-            center: mapCenter,
-            zoom: 13,
-            dragging: dragging,
-            tap: tap,
-            scrollWheelZoom: false
-        });
-
-        var Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
-            attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-            subdomains: 'abcd',
-            minZoom: 0,
-            maxZoom: 20,
-            ext: 'png'
-        });
-
-        Stamen_TonerLite.addTo(map);
-
-        map.once('focus', function () {
-            map.scrollWheelZoom.enable();
-        });
-
-        if (mapMarker) {
-            var marker = L.marker(mapCenter, {
-                icon: icon
-            }).addTo(map);
-
-            marker.bindPopup("<div class='p-4'><h5>Chihuahua City</h5><p>This is my home town!</p></div>", {
-                minwidth: 200,
-                maxWidth: 600,
-                className: 'map-custom-popup'
-            })
-
-        }
+    if ($(window).width() > 700) {
+      dragging = true;
+      tap = true;
     }
 
+    var map = L.map(mapId, {
+      center: mapCenter,
+      zoom: 13,
+      dragging: dragging,
+      tap: tap,
+      scrollWheelZoom: false,
+    });
+
+    var Stamen_TonerLite = L.tileLayer(
+      'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}',
+      {
+        attribution:
+          'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        subdomains: 'abcd',
+        minZoom: 0,
+        maxZoom: 20,
+        ext: 'png',
+      },
+    );
+
+    Stamen_TonerLite.addTo(map);
+
+    map.once('focus', function () {
+      map.scrollWheelZoom.enable();
+    });
+
+    if (mapMarker) {
+      var marker = L.marker(mapCenter, {
+        icon: icon,
+      }).addTo(map);
+
+      marker.bindPopup(
+        "<div class='p-4'><h5>Chihuahua City</h5><p>This is my home town!</p></div>",
+        {
+          minwidth: 200,
+          maxWidth: 600,
+          className: 'map-custom-popup',
+        },
+      );
+    }
+  }
 }
